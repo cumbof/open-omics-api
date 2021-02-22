265f43cf52443c2ac8d44ea0aac9b074772ce724
Fabio Cumbo <fabio.cumbo@gmail.com>
2021-02-22 11:59:06 +0100

    Update annotations.py

--- routes/annotations.py
+++ routes/annotations.py
@@ -26,7 +26,7 @@ def annotation_all(annotation_name):
     mongodb_client = getClient()
     data = {
         'annotation': 'annotation_' + annotation_name,
-        'data': get_documents(mongodb_client, annotation_name)
+        'data': get_documents(mongodb_client, 'annotation_' + annotation_name)
     }
     js = json.dumps(data, indent=4, sort_keys=True)
     resp = Response(js, status=200, mimetype='application/json')
